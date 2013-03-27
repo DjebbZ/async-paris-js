fc0d3b0a3f17701d2f7fe3fed76cbc788a6885d3
DjebbZ <khalid.jebbari@gmail.com>
2013-03-27 19:43:59 +0100

    Add first slides, with styles, but first cm-movie doesn't play...

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,23 @@
 (function() {
+
+  // impress().init();
+
   // var testTextArea = document.getElementById('test');
-  var movie = CodeMirror.movie('test');
-  movie.play();
+  var movie = CodeMirror.movie('async-callback-code');
+  // movie.play();
 
-  // var button = document.getElementById('play');
-  // button.addEventListener('click', function() {
-  //   if (movie.state() === 'play') {
-  //     movie.pause();
-  //     this.innerHTML = 'Play';
-  //   } else {
-  //     movie.play();
-  //     this.innerHTML = 'Pause';
-  //   }
-  // });
+  var button = document.getElementById('play-async-callback');
+  button.addEventListener('click', function() {
+    if (movie.state() === 'play') {
+      movie.pause();
+      this.innerHTML = 'Play';
+    } else {
+      movie.play();
+      this.innerHTML = 'Pause';
+    }
+  });
 
-  // movie.on('stop', function() {
-  //   button.innerHTML = 'Play';
-  // });
+  movie.on('stop', function() {
+    button.innerHTML = 'Play';
+  });
 })();
